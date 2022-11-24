7d6df17fbd6062860cc9479d7310f7a9ae8defd4
fu-kunn <sky53908@gmail.com>
2022-11-24 13:48:56 +0000

    [Add]15章_ignoreの設定

--- api.py
+++ api.py
@@ -1,1 +1,16 @@
-print("Hello")
+import requests
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
+URL = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/'
+API_KEY = os.environ.get('KEY')
+params = {
+  'key': API_KEY,
+  'keyword': '沖縄',
+}
+
+res = requests.get(URL, params)
+print(res.status_code)
