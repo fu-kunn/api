345777ff958f313320cfd5771e776da326447c73
fu-kunn <sky53908@gmail.com>
2022-11-29 11:41:08 +0000

    [Add]5-21APIをたたく

--- rakuten.py
+++ rakuten.py
@@ -3,3 +3,12 @@ import pandas as pd
 REQUEST_URL = 'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706'
 APP_ID = '1096144112853963195'
 
+params = {
+  'applicationId': APP_ID,
+  'format': 'json',
+  'keyword': 'バスローブ',
+  'minPrice': 10000
+}
+
+res = requests.get(REQUEST_URL, params)
+print(res.status_code)
