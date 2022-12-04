401f421e866bc8dff33501d98587f7f4721dee7a
fu-kunn <sky53908@gmail.com>
2022-12-04 00:35:58 +0000

    [Add]スプシの読み込み

--- gogle.py
+++ gogle.py
@@ -13,3 +13,12 @@ credentials = Credentials.from_service_account_file(
 )
 
 gc = gspread.authorize(credentials)
+
+# 定数の宣言
+SP_SHEET_KEY = '1d-u9f8WqlWXlsbYXTDgLkNN4M2CofoVn022Ka2OTMl4'
+# https://docs.google.com/spreadsheets/d/1d-u9f8WqlWXlsbYXTDgLkNN4M2CofoVn022Ka2OTMl4/edit#gid=0
+SP_SHEET = 'demo'
+
+# スプシの読み込み
+sh = gc.open_by_key(SP_SHEET_KEY)
+print(sh)
