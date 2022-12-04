05a35909aa73d3d74dee523f085a5f76ee118971
fu-kunn <sky53908@gmail.com>
2022-12-04 00:49:19 +0000

    [Add]スプシの整形

--- gogle.py
+++ gogle.py
@@ -1,4 +1,5 @@
 import gspread
+import pandas as pd
 
 from google.oauth2.service_account import Credentials
 
@@ -19,6 +20,14 @@ SP_SHEET_KEY = '1d-u9f8WqlWXlsbYXTDgLkNN4M2CofoVn022Ka2OTMl4'
 # https://docs.google.com/spreadsheets/d/1d-u9f8WqlWXlsbYXTDgLkNN4M2CofoVn022Ka2OTMl4/edit#gid=0
 SP_SHEET = 'demo'
 
-# スプシの読み込み
+# スプシ全体の読み込み
 sh = gc.open_by_key(SP_SHEET_KEY)
-print(sh)
+# スプシ単体の読み込み
+worksheet = sh.worksheet(SP_SHEET)
+data = worksheet.get_all_values()
+df = pd.DataFrame(data[2:], columns=data[1])
+df = df.drop(df.columns[[0]], axis=1)
+# 縦横のカラム数を返す
+test = df.shape
+# print(df)
+print(test)
